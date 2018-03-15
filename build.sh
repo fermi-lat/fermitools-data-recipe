8c8d556080ab2dbcb6e29889bb34acc0b29a7ca6
Joe Asercion <jasercion.gwu@gmail.com>
2018-03-15 14:47:51 -0400

    change install path

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 export condaname="fermitools"
 
 # Move the files into the Fermitools extFiles directory
-mkdir -p $PREFIX/share/${condaname}/data/extFiles
-cp -R v0r16 $PREFIX/share/${condaname}/data/extFiles
+mkdir -p $PREFIX/share/${condaname}/refdata/fermi
+cp -R * $PREFIX/share/${condaname}/refdata/fermi
