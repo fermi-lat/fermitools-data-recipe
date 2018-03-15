ab82801be494a255eaf05320925604974949f5c9
Joe Asercion <jasercion.gwu@gmail.com>
2018-03-15 15:17:35 -0400

    be more specific in copy command

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ export condaname="fermitools"
 
 # Move the files into the Fermitools extFiles directory
 mkdir -p $PREFIX/share/${condaname}/refdata/fermi
-cp -R * $PREFIX/share/${condaname}/refdata/fermi
+cp -R {counterpartCatalogs,galdiffuse,gaudi,jplephem,pass8Analysis}  $PREFIX/share/${condaname}/refdata/fermi
