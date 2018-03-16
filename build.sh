2fb6aa64ae33b42ff0297f61e5f4ee41729c4e0a
jasercion <jasercion.gwu@gmail.com>
2018-03-16 10:14:36 -0400

    add clone command moved from meta.yaml

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 export condaname="fermitools"
 
+git clone https://github.com/fermi-lat/fermitools-data.git
+
 # Move the files into the Fermitools extFiles directory
 mkdir -p $PREFIX/share/${condaname}/refdata/fermi
-cp -R {counterpartCatalogs,galdiffuse,jplephem,pass8Analysis}  $PREFIX/share/${condaname}/refdata/fermi
+cp -R fermitools-data/{counterpartCatalogs,galdiffuse,jplephem,pass8Analysis}  $PREFIX/share/${condaname}/refdata/fermi
