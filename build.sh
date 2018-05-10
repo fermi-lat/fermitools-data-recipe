a39a4c12ccab4062b3329e63a99ad234a83b9975
jasercion <joseph.a.asercion@nasa.gov>
2018-05-10 17:33:28 -0400

    add -R flag to cp command

--- build.sh
+++ build.sh
@@ -12,5 +12,5 @@ cp -R fermitools-data/{counterpartCatalogs,galdiffuse,jplephem,pass8Analysis}  $
 for i in "${tooldirs[@]}"
 do
 mkdir -p $PREFIX/share/${condaname}/data/$i/outref
-cp -n fermitools-data/outrefFiles/$i/* $PREFIX/share/${condaname}/data/$i/outref/
+cp -R -n fermitools-data/outrefFiles/$i/* $PREFIX/share/${condaname}/data/$i/outref/
 done
